9006106e545dc6160e2e0328bf3e0412de1911fb
turanraftamim <turanrafsan@gmail.com>
2025-07-30 23:42:57 +0600

    Push

---
+++ src/main/java/turan/PostJob.java
@@ -0,0 +1,32 @@
+package turan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class PostJob
+{
+    @javafx.fxml.FXML
+    private TextField jobpost;
+    @javafx.fxml.FXML
+    private RadioButton parttimerb;
+    @javafx.fxml.FXML
+    private DatePicker applideadlinedp;
+    @javafx.fxml.FXML
+    private RadioButton fulltimerb;
+    @javafx.fxml.FXML
+    private ComboBox deptcb;
+    @javafx.fxml.FXML
+    private ComboBox experiencecb;
+    @javafx.fxml.FXML
+    private TextField jobdescriptiontextbox;
+    @javafx.fxml.FXML
+    private Label errormsglabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void postjobhandle(ActionEvent actionEvent) {
+    }
+}
