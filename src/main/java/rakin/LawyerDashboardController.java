9e2fb6513745747fc84eb088bc349e69538a38b4
Amanta0-0 <158179923+RakinUrRahman@users.noreply.github.com>
2025-07-30 21:14:53 +0600

    Lawyer Dashboard

---
+++ src/main/java/rakin/LawyerDashboardController.java
@@ -0,0 +1,40 @@
+package rakin;
+
+public class LawyerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void logout(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void requestExpert(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void openMessages(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void joinHearing(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void openDraft(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void openCaseList(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void closeCase(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitReport(ActionEvent actionEvent) {
+    }
+}
