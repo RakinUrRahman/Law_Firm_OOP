5434b3cb66eb3aa82134470beb22b2bdd9c72d45
turanraftamim <turanrafsan@gmail.com>
2025-07-31 00:48:21 +0600

    Push

---
+++ src/main/java/turan/EmployeeAttendace.java
@@ -0,0 +1,44 @@
+package turan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class EmployeeAttendace
+{
+    @javafx.fxml.FXML
+    private TableView attendanceTV;
+    @javafx.fxml.FXML
+    private TextField employeenametf;
+    @javafx.fxml.FXML
+    private TableColumn departmentTVC;
+    @javafx.fxml.FXML
+    private TableColumn dateTVC;
+    @javafx.fxml.FXML
+    private TableColumn statusTVC;
+    @javafx.fxml.FXML
+    private TableColumn employeenameTVC;
+    @javafx.fxml.FXML
+    private TableColumn employeeidTVC;
+    @javafx.fxml.FXML
+    private ComboBox statuscb;
+    @javafx.fxml.FXML
+    private DatePicker datedp;
+    @javafx.fxml.FXML
+    private ComboBox deptcb;
+    @javafx.fxml.FXML
+    private TextField employeeidtf;
+    @javafx.fxml.FXML
+    private Label errormsglabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backbuttonhandle(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void attendancebuttonhandle(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/turan/InterviewSchedule.java
@@ -0,0 +1,48 @@
+package turan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class InterviewSchedule
+{
+    @javafx.fxml.FXML
+    private TableColumn interviewtypeTVC;
+    @javafx.fxml.FXML
+    private TextField candidatetb;
+    @javafx.fxml.FXML
+    private DatePicker interviewdp;
+    @javafx.fxml.FXML
+    private RadioButton onlinerb;
+    @javafx.fxml.FXML
+    private TableColumn positionTVC;
+    @javafx.fxml.FXML
+    private ComboBox positioncb;
+    @javafx.fxml.FXML
+    private ComboBox interviewercb;
+    @javafx.fxml.FXML
+    private TableColumn candidatenameTVC;
+    @javafx.fxml.FXML
+    private RadioButton inpersonrb;
+    @javafx.fxml.FXML
+    private TableColumn DateTVC;
+    @javafx.fxml.FXML
+    private TableView interviewTV;
+    @javafx.fxml.FXML
+    private Label errormsglabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backbuttonhandle(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void schedulebuttonhandle(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void editbuttonhandle(ActionEvent actionEvent) {
+    }
+}
